cc2a60b02578fbee05d8fdf3e667409631544a66
xth87706565 <david.hong20010113@gmail.com>
2023-09-01 23:26:55 -0700

    new commit

--- python flask project/website/static/index.js
+++
@@ -1,9 +0,0 @@
-function deleteNote(noteid) {
-    const mystr=noteid.toString();
-    fetch("/deletenote", {
-      method: "POST",
-      body: JSON.stringify({ "whatthefuck": mystr }),
-    }).then((_res) => {
-      window.location.href = "/";
-    });
-  }
